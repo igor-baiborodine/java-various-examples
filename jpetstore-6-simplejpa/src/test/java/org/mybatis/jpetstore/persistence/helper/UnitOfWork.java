14fe11357dff0c245c848c9c658af4a561b8a4c5
Igor Baiborodine <ibaiborodine@gmail.com>
2015-01-07 22:05:26 -0500

    Remove redundant classes.

--- jpetstore-6-simplejpa/src/test/java/org/mybatis/jpetstore/persistence/helper/UnitOfWork.java
+++
@@ -1,8 +0,0 @@
-package org.mybatis.jpetstore.persistence.helper;
-
-/**
- * Reference: https://github.com/npryce/goos-code-examples
- */
-public interface UnitOfWork {
-    void work() throws Exception;
-}
