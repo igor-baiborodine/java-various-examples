186571ee4db1c3ed4cfcb745f76fbf628dcb5c37
Igor Baiborodine <ibaiborodine@gmail.com>
2015-01-18 22:06:48 -0500

    Add refactoring type inference example.

---
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic1_3/TypeInferenceExample.java
@@ -0,0 +1,28 @@
+package com.kiroule.ocpupgradejava8.topic1_3;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * @author Igor Baiborodine
+ */
+public class TypeInferenceExample {
+
+    public static void main(String... args) {
+
+        Foo<Bar> foo = new Foo<>();
+
+        foo.<String>apply(bar -> bar::setBar);
+        // Or we can omit the type String, a Java compiler automatically infers
+        // (from the method's argument) that the type parameter is String:
+        foo.apply(bar -> bar::setBar);
+    }
+}
+
+class Foo<T> {
+    <U> void apply(Function<T, Consumer<U>> bar) {}
+}
+
+class Bar {
+    void setBar(String bar){}
+}
