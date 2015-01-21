9d42a2e9857481f6937f5bd7a3af644ffaa821c2
Igor Baiborodine <ibaiborodine@gmail.com>
2015-01-20 22:27:08 -0500

    Misc refactorings.

--- ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic1_2/FunctionalInterfaceExample.java
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic1_2/FunctionalInterfaceExample.java
@@ -3,7 +3,7 @@ package com.kiroule.ocpupgradejava8.topic1_2;
 /**
  * @author Igor Baiborodine
  */
-public class FunctionalInterfaceHelloWorld {
+public class FunctionalInterfaceExample {
 
     public static void process(HelloWorld hw) {
         hw.printGreeting();
@@ -11,18 +11,18 @@ public class FunctionalInterfaceHelloWorld {
 
     public static void main(String... args) {
 
-        HelloWorld hw1 = () -> System.out.println("Hello World 1");
+        HelloWorld hw1 = () -> System.out.println("Hello World 1!");
 
         HelloWorld hw2 = new HelloWorld() {
             @Override
             public void printGreeting() {
-                System.out.println("Hello World 2");
+                System.out.println("Hello World 2!");
             }
         };
 
         process(hw1);
         process(hw2);
-        process(() -> System.out.println("Hello World 3"));
+        process(() -> System.out.println("Hello World 3!"));
     }
 }
 
