80506cd7169364b5519337aaa664e340bc53f721
Igor Baiborodine <ibaiborodine@gmail.com>
2015-01-21 19:51:29 -0500

    Misc refactorings.

--- ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic1_3/TypeInferenceExample.java
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic1_3/TypeInferenceExample.java
@@ -14,7 +14,7 @@ public class TypeInferenceExample {
 
         foo.<String>apply(bar -> bar::setBar);
         // We omit the type String and Java compiler automatically infers
-        // (from the method's argument) that the type parameter is String:
+        // (from the method's argument) that the type parameter is String
         foo.apply(bar -> bar::setBar);
     }
 }
