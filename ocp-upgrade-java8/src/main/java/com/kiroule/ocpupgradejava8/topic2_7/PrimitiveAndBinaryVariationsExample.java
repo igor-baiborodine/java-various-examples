51c8f99181a991e8fb761b099eb17be1748a0fde
Igor Baiborodine <ibaiborodine@gmail.com>
2015-01-23 23:42:31 -0500

    Add primitive and binary variations example.

--- ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic2_7/PrimitiveAndBinaryVariationsExample.java
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic2_7/PrimitiveAndBinaryVariationsExample.java
@@ -2,7 +2,10 @@ package com.kiroule.ocpupgradejava8.topic2_7;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntConsumer;
 
 /**
  * @author Igor Baiborodine
@@ -11,15 +14,24 @@ public class PrimitiveAndBinaryVariationsExample {
 
     public static void main(String... args) {
 
-        BiFunction<String, String, Person> bf = Person::new;
+        BiFunction<String, String, Person> bf = (firstName, lastName) -> new Person(firstName, lastName);
 
         List<Person> persons = Arrays.asList(
                 bf.apply("Bender", "Rodriguez"),
                 bf.apply("Philip", "Fry"),
                 bf.apply("Turanga", "Leela"));
 
-    }
+        BiConsumer<String, String> biConsumer = (firstName, lastName) -> System.out.println(lastName.toUpperCase() + ", " + firstName);
+        for (Person person : persons) {
+            biConsumer.accept(person.getFirstName(), person.getLastName());
+        }
 
+        IntBinaryOperator intBinaryOperator = (s1, s2) -> s1 * s2;
+        int side1 = 3, side2 = 8;
+        IntConsumer intConsumer = (area) -> System.out.println("Area for rectangle with sides["
+                + side1 + ", " + side2 + "] is " + area);
+        intConsumer.accept(intBinaryOperator.applyAsInt(side1, side2));
+    }
 }
 
 class Person {
