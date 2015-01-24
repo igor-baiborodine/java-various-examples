836c3c43d4309a62134d562b37296fd2cf61d934
Igor Baiborodine <ibaiborodine@gmail.com>
2015-01-23 23:04:36 -0500

    Add primitive and binary variations example.

---
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic2_7/PrimitiveAndBinaryVariationExample.java
@@ -0,0 +1,42 @@
+package com.kiroule.ocpupgradejava8.topic2_7;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+
+/**
+ * @author Igor Baiborodine
+ */
+public class PrimitiveAndBinaryVariationExample {
+
+    public static void main(String... args) {
+
+        BiFunction<String, String, Person> bf = Person::new;
+
+        List<Person> persons = Arrays.asList(
+                bf.apply("Bender", "Rodriguez"),
+                bf.apply("Philip", "Fry"),
+                bf.apply("Turanga", "Leela"));
+
+    }
+
+}
+
+class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
