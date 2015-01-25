973a8a3a74e0ca99e7ef4a1bbc0b68de708b25c1
Igor Baiborodine <ibaiborodine@gmail.com>
2015-01-24 21:03:21 -0500

    Add forEach example.

---
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic3_1/ForEachExample.java
@@ -0,0 +1,54 @@
+package com.kiroule.ocpupgradejava8.topic3_1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+
+/**
+ * @author Igor Baiborodine
+ */
+public class ForEachExample {
+
+    public static void main(String... args) {
+
+        BiFunction<String, String, Person> bf = Person::new; // method reference
+
+        List<Person> persons = Arrays.asList(
+                bf.apply("Philip", "Fry"),
+                bf.apply("Bender", "Rodriguez"),
+                bf.apply("Turanga", "Leela"));
+
+        // Java 7
+        for (Person person : persons) {
+            person.printFullName();
+        }
+
+        // Java 8
+        persons.forEach(Person::printFullName);
+        persons.forEach(p -> System.out.println(p.getFirstName() + " " + p.getLastName().toUpperCase()));
+    }
+}
+
+class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void printFullName() {
+        System.out.println(lastName + ", " + firstName);
+    }
+}
+
