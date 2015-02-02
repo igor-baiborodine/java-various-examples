2a90d793df3dbb4f2504b0f812369bded0d45b55
Igor Baiborodine <ibaiborodine@gmail.com>
2015-02-01 19:09:38 -0500

    Add parallel stream example.

--- ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic5_1/ParallelStreamExample.java
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic5_1/ParallelStreamExample.java
@@ -9,10 +9,11 @@ public class ParallelStreamExample {
 
     public static void main(String... args) {
 
+        int minValue = 1;
         int maxValue = 100_000;
-        long sum = IntStream.range(1, maxValue)
+        long sum = IntStream.range(minValue, maxValue)
                 .parallel()
                 .sum();
-        System.out.printf("Sum[1, %d]: %d", maxValue, sum);
+        System.out.printf("Sum[%d, %d]: %d", minValue, maxValue, sum);
     }
 }
