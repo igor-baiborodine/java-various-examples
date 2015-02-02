3b6c7986f9989c1a8f5691bbb316c541529bb4a2
Igor Baiborodine <ibaiborodine@gmail.com>
2015-02-01 19:08:04 -0500

    Add parallel stream example.

--- ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic5_1/ParallelStreamExample.java
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic5_1/ParallelStreamExample.java
@@ -1,9 +1,18 @@
 package com.kiroule.ocpupgradejava8.topic5_1;
 
+import java.util.stream.IntStream;
+
 /**
  * @author Igor Baiborodine
  */
 public class ParallelStreamExample {
 
-    // TODO: implement me
+    public static void main(String... args) {
+
+        int maxValue = 100_000;
+        long sum = IntStream.range(1, maxValue)
+                .parallel()
+                .sum();
+        System.out.printf("Sum[1, %d]: %d", maxValue, sum);
+    }
 }
