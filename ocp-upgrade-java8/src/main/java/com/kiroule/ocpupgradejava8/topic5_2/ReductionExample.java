e032f7f9d97898dba96171632e881c8d6a0db350
Igor Baiborodine <ibaiborodine@gmail.com>
2015-02-01 19:33:13 -0500

    Add reduction example.

--- ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic5_2/ReductionExample.java
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic5_2/ReductionExample.java
@@ -1,9 +1,45 @@
 package com.kiroule.ocpupgradejava8.topic5_2;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Igor Baiborodine
  */
 public class ReductionExample {
 
-    // TODO: implement me 
+    public static void main(String... args) {
+
+        List<FuturamaCharacter> characters = Arrays.asList(
+                new FuturamaCharacter("Bender", "Rodriguez", 5),
+                new FuturamaCharacter("Philip", "Fry", 126),
+                new FuturamaCharacter("Turanga", "Leela", 22));
+        System.out.println("Futurama characters:");
+        characters.forEach(c -> System.out.println(c));
+
+        int sumAge = characters
+                .stream()
+                .parallel()
+                .mapToInt(FuturamaCharacter::getAge).sum();
+        System.out.println("\nSum of ages: " + sumAge);
+    }
+}
+
+class FuturamaCharacter {
+    private String firstName;
+    private String lastName;
+    private int age = 0;
+
+    public FuturamaCharacter(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int getAge() { return age; }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " [" + age + "]";
+    }
 }
