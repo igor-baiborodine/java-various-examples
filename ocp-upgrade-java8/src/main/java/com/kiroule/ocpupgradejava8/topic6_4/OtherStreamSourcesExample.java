ac9a935be5bbd6b924f7e34011b7c0d384af5e98
Igor Baiborodine <ibaiborodine@gmail.com>
2015-02-04 19:39:27 -0500

    Add other stream sources example.

--- ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic6_4/OtherStreamSourcesExample.java
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic6_4/OtherStreamSourcesExample.java
@@ -1,9 +1,27 @@
 package com.kiroule.ocpupgradejava8.topic6_4;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * @author Igor Baiborodine
  */
 public class OtherStreamSourcesExample {
 
-    // TODO: implement me
+    public static void main(String... args) {
+
+        String[] words = {"sporadic", "perjury", "confounded", "skimming", "incumbent", "jailer"};
+
+        Stream<String> stream = Stream.of(words);
+        System.out.println("Words in upper case: ");
+        stream.map(String::toUpperCase).forEach(System.out::println);
+
+        System.out.printf("%nWords count: %d%n", Arrays.stream(words).count());
+
+        int minValue = 1;
+        int maxValue = 100_000;
+        long sum = IntStream.range(minValue, maxValue).sum();
+        System.out.printf("%nSum[%d, %d]: %d", minValue, maxValue, sum);
+    }
 }
