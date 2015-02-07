f9e11400a7a0e45a44e36f89b86c6adbb398c862
Igor Baiborodine <ibaiborodine@gmail.com>
2015-02-07 17:02:46 -0500

    Add Nashorn examples.

---
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic9_1/JavasriptUsesJavaExample.java
@@ -0,0 +1,9 @@
+package com.kiroule.ocpupgradejava8.topic9_1;
+
+/**
+ * @author Igor Baiborodine
+ */
+public class JavasriptUsesJavaExample {
+
+    // TODO: implement me
+}
---
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic9_2/JavaEvaluatesJavascriptExample.java
@@ -0,0 +1,9 @@
+package com.kiroule.ocpupgradejava8.topic9_2;
+
+/**
+ * @author Igor Baiborodine
+ */
+public class JavaEvaluatesJavascriptExample {
+
+    // TODO: implement me
+}
