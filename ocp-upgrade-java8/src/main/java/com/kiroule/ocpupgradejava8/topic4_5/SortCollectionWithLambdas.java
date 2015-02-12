d654199c44d82efa0b1a2c15f3e57ec925919a08
Igor Baiborodine <ibaiborodine@gmail.com>
2015-02-11 19:51:28 -0500

    Misc refactorings.

--- ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic4_5/SortCollectionWithLambdas.java
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic4_5/SortCollectionWithLambdas.java
@@ -19,10 +19,12 @@ public class SortCollectionWithLambdas {
                 new FuturamaCharacter("Cubert", "Farnsworth", 12),
                 new FuturamaCharacter("Turanga", "Leela", 22),
                 new FuturamaCharacter("Hubert", "Farnsworth", 161));
+
         System.out.println("Futurama characters:");
         characters.forEach(System.out::println);
 
         // Java 7
+        System.out.println("\nSorted by last name ASC (Java 7):");
         Comparator<FuturamaCharacter> lastNameAscComparator = new Comparator<FuturamaCharacter>() {
             @Override
             public int compare(FuturamaCharacter c1, FuturamaCharacter c2) {
@@ -30,34 +32,34 @@ public class SortCollectionWithLambdas {
             }
         };
         Collections.sort(characters, lastNameAscComparator);
-        System.out.println("\nSorted by last name ASC (Java 7):");
         characters.forEach(System.out::println);
 
         // Java 8
-        Comparator<FuturamaCharacter> firstNameAscComparator = (c1, c2) -> c1.getFirstName().compareTo(c2.getFirstName());
-        Collections.sort(characters, firstNameAscComparator);
         System.out.println("\nSorted by first name ASC (Java 8):");
+        Comparator<FuturamaCharacter> firstNameAscComparator =
+                (c1, c2) -> c1.getFirstName().compareTo(c2.getFirstName());
+        Collections.sort(characters, firstNameAscComparator);
         characters.forEach(System.out::println);
 
-        Collections.sort(characters, comparing(FuturamaCharacter::getAge));
         System.out.println("\nSorted by age ASC (Java 8):");
+        Collections.sort(characters, comparing(FuturamaCharacter::getAge));
         characters.forEach(System.out::println);
 
-        characters.sort((c1, c2) -> c1.getLastName().compareTo(c2.getLastName()));
         System.out.println("\nSorted by last name ASC (Java 8):");
+        characters.sort((c1, c2) -> c1.getLastName().compareTo(c2.getLastName()));
         characters.forEach(System.out::println);
 
-        characters.sort(comparing(FuturamaCharacter::getFirstName));
         System.out.println("\nAgain sorted by first name ASC (Java 8):");
+        characters.sort(comparing(FuturamaCharacter::getFirstName));
         characters.forEach(System.out::println);
 
+        System.out.println("\nSorted by last name ASC and first name DESC (Java 8):");
         Comparator<FuturamaCharacter> lastNameAscAndFirstNameDescComparator =
                 comparing(FuturamaCharacter::getLastName)
                         // DESC
                         .thenComparing((c1, c2) -> c2.getFirstName().compareTo(c1.getFirstName()));
         characters.sort(lastNameAscAndFirstNameDescComparator);
-        System.out.println("\nSorted by last name ASC and first name DESC (Java 8):");
-        characters.forEach(System.out::println);
+        characters.forEach(c -> System.out.println(c.getLastName() + ", " + c.getFirstName()));
     }
 }
 
