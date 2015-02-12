4ee086b7ddd3b3397a0bc2dac40bf580eb532fb5
Igor Baiborodine <ibaiborodine@gmail.com>
2015-02-11 21:33:42 -0500

    Misc refactorings.

--- ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic5_2/ReductionExample.java
+++ ocp-upgrade-java8/src/main/java/com/kiroule/ocpupgradejava8/topic5_2/ReductionExample.java
@@ -14,14 +14,23 @@ public class ReductionExample {
                 new FuturamaCharacter("Bender", "Rodriguez", 5),
                 new FuturamaCharacter("Philip", "Fry", 126),
                 new FuturamaCharacter("Turanga", "Leela", 22));
+
         System.out.println("Futurama characters:");
         characters.forEach(c -> System.out.println(c));
 
         int sumAge = characters
                 .stream()
                 .parallel()
-                .mapToInt(FuturamaCharacter::getAge).sum();
+                .mapToInt(FuturamaCharacter::getAge)
+                .sum();
         System.out.println("\nSum of ages: " + sumAge);
+
+        int sumAgeReduce = characters
+                .stream()
+                .parallel()
+                .map(FuturamaCharacter::getAge)
+                .reduce(0, (a, b) -> a + b);
+        System.out.println("\nSum of ages with reduce: " + sumAgeReduce);
     }
 }
 
