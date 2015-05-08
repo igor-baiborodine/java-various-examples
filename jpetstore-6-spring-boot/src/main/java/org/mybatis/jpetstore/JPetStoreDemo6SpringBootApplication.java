c6837026f8731be0b7602b9b26622946199da51d
Igor Baiborodine <ibaiborodine@gmail.com>
2015-05-07 22:45:12 -0400

    Move web config to main application class

--- jpetstore-6-spring-boot/src/main/java/org/mybatis/jpetstore/JPetStoreDemo6SpringBootApplication.java
+++ jpetstore-6-spring-boot/src/main/java/org/mybatis/jpetstore/JPetStoreDemo6SpringBootApplication.java
@@ -1,18 +1,28 @@
 package org.mybatis.jpetstore;
 
+import net.sourceforge.stripes.controller.StripesFilter;
+
 import org.mybatis.jpetstore.config.DataSourceConfig;
 import org.mybatis.jpetstore.config.ServiceConfig;
-import org.mybatis.jpetstore.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+import java.util.EnumSet;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import static javax.servlet.DispatcherType.REQUEST;
+
 @SpringBootApplication
 public class JPetStoreDemo6SpringBootApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
-    SpringApplication.run(new Class[] {JPetStoreDemo6SpringBootApplication.class, WebConfig.class}, args);
+    SpringApplication.run(new Class[] {JPetStoreDemo6SpringBootApplication.class}, args);
   }
 
   @Override
@@ -20,6 +30,26 @@ public class JPetStoreDemo6SpringBootApplication extends SpringBootServletInitia
     return application.sources(
         DataSourceConfig.class,
         ServiceConfig.class);
+  }
+
+  @Override
+  public void onStartup(ServletContext servletContext) throws ServletException {
+
+    servletContext.setInitParameter("contextConfigLocation", "<NONE>");
+    servletContext.setInitParameter(
+        "javax.servlet.jsp.jstl.fmt.localizationContext", "StripesResources");
+
+    FilterRegistration.Dynamic stripesFilter = servletContext.addFilter(
+        "stripesFilter", StripesFilter.class);
+    stripesFilter.setInitParameter("ActionResolver.Packages", "org.mybatis.jpetstore.web");
+    stripesFilter.setInitParameter(
+        "Interceptor.Classes", "net.sourceforge.stripes.integration.spring.SpringInterceptor");
+    stripesFilter.addMappingForServletNames(EnumSet.of(REQUEST), true, "stripesDispatcher");
+
+    ServletRegistration.Dynamic stripesDispatcher = servletContext.addServlet(
+        "stripesDispatcher", new net.sourceforge.stripes.controller.DispatcherServlet());
+    stripesDispatcher.setLoadOnStartup(1);
+    stripesDispatcher.addMapping("*.action");
   }
 
 }
