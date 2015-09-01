8ea90511b1ff2eda3f6bbbcb935d05791bee305e
Igor Baiborodine <ibaiborodine@gmail.com>
2015-08-31 21:23:35 -0400

    Initial commit

---
+++ jpetstore-6-vaadinspring/src/main/java/com/kiroule/jpetstore/vaadinspring/Jpetstore6VaadinspringApplication.java
@@ -0,0 +1,12 @@
+package com.kiroule.jpetstore.vaadinspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpetstore6VaadinspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpetstore6VaadinspringApplication.class, args);
+    }
+}
---
+++ jpetstore-6-vaadinspring/src/test/java/com/kiroule/jpetstore/vaadinspring/Jpetstore6VaadinspringApplicationTests.java
@@ -0,0 +1,18 @@
+package com.kiroule.jpetstore.vaadinspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Jpetstore6VaadinspringApplication.class)
+@WebAppConfiguration
+public class Jpetstore6VaadinspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
